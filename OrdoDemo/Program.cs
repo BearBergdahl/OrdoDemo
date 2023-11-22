599b51d3b2a7f0bc46a7c7aca8ec887a63bf8769
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2023-11-22 10:12:50 +0100

    Add project files.

---
+++ OrdoDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Diagnostics;
+
+namespace OrdoDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] arr = GenerateArray();
+            
+            Stopwatch sw = Stopwatch.StartNew();
+            int[] quickArr = SortingService.QuickSort(arr, 0, arr.Length - 1);
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+
+            int[] arr2 = GenerateArray();
+            Stopwatch sw2 = Stopwatch.StartNew();
+            int[] sortArr = SortingService.InsertionSort(arr2);
+            sw2.Stop();
+            Console.WriteLine(sw2.Elapsed);
+            
+        }
+
+        private static int[] GenerateArray()
+        {
+            int limit = 10000000;
+            Random random = new Random();
+            int[] arr = new int[limit];
+            for (int i = 0; i < limit ; i++)
+            {
+                arr[i] = random.Next(0,1000);
+            }
+            return arr;
+        }
+    }
+}
---
+++ OrdoDemo/SortingService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdoDemo
+{
+    public static class SortingService
+    {
+
+        public static int[] InsertionSort(int[] arr)
+        {
+            int n1 = arr.Length;
+            int n2 = 0;
+            for (int i = 1; i < n1; ++i)
+            {
+                int key = arr[i];
+                n2 = i - 1;
+                while (n2 >= 0 && arr[n2] > key)
+                {
+                    arr[n2 + 1] = arr[n2];
+                    n2 = n2 - 1;
+                }
+                arr[n2 + 1] = key;
+            }
+            return arr;
+        }
+
+        public static int[] QuickSort(int[] arr, int begin, int end)
+        {
+            if (begin < end)
+            {
+                int partitionIndex = Partition(arr, begin, end);
+                //Recursive
+                QuickSort(arr, begin, partitionIndex - 1);
+                QuickSort(arr, partitionIndex + 1, end);
+            }
+            return arr;
+        }
+
+        public static int Partition(int[] arr, int begin, int end)
+        {
+            int pivot = arr[end];
+            int i = (begin - 1);
+            int swapTemp = 0;
+            for (int j = begin; j < end; j++)
+            {
+                if (arr[j] <= pivot)
+                {
+                    i++;
+
+                    swapTemp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = swapTemp;
+                }
+            }
+
+            swapTemp = arr[i + 1];
+            arr[i + 1] = arr[end];
+            arr[end] = swapTemp;
+
+            return i + 1;
+        }
+    }
+}
+
